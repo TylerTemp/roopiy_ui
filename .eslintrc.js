550f0746127910ea261230e363c7baaf5c89fd19
TylerTemp <tylertempdev@gmail.com>
2023-10-16 08:25:00 +0100

    Exit page close database

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,15 @@ module.exports = {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': [
+        "warn",
+        {
+            "ignoreRestSiblings": true,
+            "argsIgnorePattern": "^_",
+            "varsIgnorePattern": "^_",
+            "caughtErrorsIgnorePattern": "^_"
+        }
+    ],
     'prettier/prettier': 0,
     'react/function-component-definition': [
       0
